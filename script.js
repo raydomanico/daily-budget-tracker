d6db4b07dd622782e002637a7d6fa1179bc9cfad
Raymond Domanico <r.domanicoo09@gmail.com>
2026-05-11 06:07:25 +0800

    Add PWA support - manifest and service worker

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js');
+}
+
 const DAILY_LIMIT=250;
 const CATEGORY_SALARY="Salary";
 const CATEGORY_DAILY_SAVINGS="Daily Budget Savings";
